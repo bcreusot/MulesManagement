046efd662b6a80581947a6c1e3f3111903b1bde8
Benjamin Creusot <bcreusot@gmail.com>
2014-04-21 16:41:14 +0200

    Version 2.0
    Addition of others selections items
    Correction of some bugs :
    - stack bugs
    - item name bug

--- Langage.lua
+++ Langage.lua
@@ -1,75 +1,123 @@
 
 
-MM_CHARACTER_TYPE_MAIN = 100
-MM_CHARACTER_TYPE_MULE = 101
-
-MM_ITEMS_FILTER_ALL    = 200
-MM_ITEMS_FILTER_CRAFT  = 201
-MM_ITEMS_FILTER_OTHERS = 202
-
-
 langage = {
 	
 	["English"] = {
 		--Option Translation
+		version							= "Version",
 		title							= "Mules Management",
 		reloadWarning					= "The UI will reload",
 		dropDownLangageText				= "Langage",
 		dropDownLangageTooltip			= "Select your langage",
+		spamChatText					= "Chat Spam",
+		spamChatTooltip					= "Display moved items in the chat",
 		dropDownCharacterTypeText		= "Character Type",
 		dropDownCharacterTypeTooltip	= "Select the role of your character",
-		dropDownItemsFilterText			= "Items Filter",
-		dropDownItemsFilterTooltip		= "Select the categories of items",
-		craftCheckBoxTooltipMain		= "Send to Bank",
-		craftCheckBoxTooltipMule		= "Send to Inventory",
+		AllText 						= "Send all",
+		AllTooltip						= "NO filter ! Send All!",
+		checkBoxTooltipMain				= "Send to Bank",
+		checkBoxTooltipMule				= "Send to Inventory",
 		itemsMoved						= "item(s) moved",
+		itemsStacked					= "item(s) stacked",
 
 		--Types of charaters
-		[MM_CHARACTER_TYPE_MAIN]		= "Main",
-		[MM_CHARACTER_TYPE_MULE]		= "My Mule",
-		
-		--Types of filters
-		[MM_ITEMS_FILTER_ALL]  			= "All Items",
-		[MM_ITEMS_FILTER_CRAFT]			= "Craft Items",
-		[MM_ITEMS_FILTER_OTHERS]		= "Others Items",
+		MM_CHARACTER_TYPE_MAIN			= "Main",
+		MM_CHARACTER_TYPE_MULE			= "My Mule",
 
-		--Types of filter : craft
-		[CRAFTING_TYPE_BLACKSMITHING]	= "Blacksmithing",
-		[CRAFTING_TYPE_CLOTHIER]		= "Clothier",
-		[CRAFTING_TYPE_ENCHANTING]		= "Enchanting",
-		[CRAFTING_TYPE_ALCHEMY]			= "Alchemy",
-		[CRAFTING_TYPE_PROVISIONING]	= "Provisioning",
-		[CRAFTING_TYPE_WOODWORKING]		= "Woodworking"
+		--CRAFT
+		craftHeader						= "CRAFT",
+		--Types of craft
+		CRAFTING_TYPE_BLACKSMITHING		= "Blacksmithing",
+		CRAFTING_TYPE_CLOTHIER			= "Clothier",
+		CRAFTING_TYPE_ENCHANTING		= "Enchanting",
+		CRAFTING_TYPE_ALCHEMY			= "Alchemy",
+		CRAFTING_TYPE_PROVISIONING		= "Provisioning",
+		CRAFTING_TYPE_WOODWORKING		= "Woodworking",
+		CRAFTING_TYPE_RAW				= "Raw Materials",
+		ITEMTYPE_STYLE_MATERIAL			= "Style Materials",			
+		ITEMTYPE_WEAPON_TRAIT			= "Weapon Traits",			
+		ITEMTYPE_ARMOR_TRAIT			= "Armor Traits",			
+
+		--OTHERS
+		othersHeader					= "OTHERS",
+		--type of others
+		ITEMTYPE_WEAPON					= "Weapons",	
+		ITEMTYPE_WEAPON_BOOSTER			= "Weapon boosters",			
+		ITEMTYPE_ARMOR					= "Armors",	
+		ITEMTYPE_ARMOR_BOOSTER			= "Armor boosters",		
+		ITEMTYPE_COSTUME				= "Costumes",		
+		ITEMTYPE_DISGUISE				= "Disguises",		
+		ITEMTYPE_DRINK					= "Drinks",	
+		ITEMTYPE_FOOD					= "Foods",	
+		ITEMTYPE_AVA_REPAIR				= "Repair Items",		
+		ITEMTYPE_LOCKPICK				= "Lockpicks",		
+		ITEMTYPE_POTION					= "Potions",	
+		ITEMTYPE_POISON					= "Poisons",	
+		ITEMTYPE_RECIPE					= "Recipes",	
+		ITEMTYPE_SCROLL					= "Scrolls",	
+		ITEMTYPE_SIEGE					= "Sieges",	
+		ITEMTYPE_SOUL_GEM				= "Soul Gems",		
+		ITEMTYPE_TABARD					= "Tabards",	
+		ITEMTYPE_TROPHY					= "Trophies"	
 	},
 	["Francais"] = {
 		--Traduction dans les options
+		version							= "Version",
 		title							= "Management de Mules",
 		reloadWarning					= "L'affichage va recharger",
 		dropDownLangageText				= "Langue",
 		dropDownLangageTooltip			= "Selectionnez votre langue",
+		spamChatText					= "Spam du chat",
+		spamChatTooltip					= "Affiche les items deplacés dans le chat",
 		dropDownCharacterTypeText		= "Type de Personnage",
 		dropDownCharacterTypeTooltip	= "Selectionnez le rôle de votre Personnage",
-		dropDownItemsFilterText			= "Filtre sur les objets",
-		dropDownItemsFilterTooltip		= "Selectionnez la catégorie d'objets à filter",
-		craftCheckBoxTooltipMain		= "Envoyer à la banque",
-		craftCheckBoxTooltipMule		= "Envoyer dans l'inventaire",
+		AllText 						= "Envoyer tout",
+		AllTooltip						= "Aucun filtre !",
+
+
+		checkBoxTooltipMain				= "Envoyer à la banque",
+		checkBoxTooltipMule				= "Envoyer dans l'inventaire",
 		itemsMoved						= "objet(s) deplacé(s)",
+		itemsStacked					= "objet(s) stacké(s)",
 
 		--Types de personnages
-		[MM_CHARACTER_TYPE_MAIN]		= "Main - Principal",
-		[MM_CHARACTER_TYPE_MULE]		= "Ma Mule",
-		[MM_ITEMS_FILTER_OTHERS]		= "Divers",
-
-		--Types de filtres
-		[MM_ITEMS_FILTER_ALL]  			= "Tous les objets",
-		[MM_ITEMS_FILTER_CRAFT]			= "Objets de Craft",
-
+		MM_CHARACTER_TYPE_MAIN			= "Main - Principal",
+		MM_CHARACTER_TYPE_MULE			= "Ma Mule",
+		
+		--CRAFT
+		craftHeader						= "CRAFT",
 		--Types de filtres : craft
-		[CRAFTING_TYPE_BLACKSMITHING]	= "Forge",
-		[CRAFTING_TYPE_CLOTHIER]		= "Couture",
-		[CRAFTING_TYPE_ENCHANTING]		= "Enchantement",
-		[CRAFTING_TYPE_ALCHEMY]			= "Alchimie",
-		[CRAFTING_TYPE_PROVISIONING]	= "Cuisine",
-		[CRAFTING_TYPE_WOODWORKING]		= "Travail du bois"
+		CRAFTING_TYPE_BLACKSMITHING		= "Forge",
+		CRAFTING_TYPE_CLOTHIER			= "Couture",
+		CRAFTING_TYPE_ENCHANTING		= "Enchantement",
+		CRAFTING_TYPE_ALCHEMY			= "Alchimie",
+		CRAFTING_TYPE_PROVISIONING		= "Cuisine",
+		CRAFTING_TYPE_WOODWORKING		= "Travail du bois",
+		CRAFTING_TYPE_RAW				= "Matériaux bruts",
+		ITEMTYPE_STYLE_MATERIAL			= "Matériaux de Styles",			
+		ITEMTYPE_WEAPON_TRAIT			= "Caractéristiques d'arme",			
+		ITEMTYPE_ARMOR_TRAIT			= "Caractéristiques d'armure",	
+
+		--DIVERS
+		othersHeader					= "DIVERS",
+		--type of others
+		ITEMTYPE_WEAPON					= "Armes",	
+		ITEMTYPE_WEAPON_BOOSTER			= "Boosters d'arme",			
+		ITEMTYPE_ARMOR					= "Armures",	
+		ITEMTYPE_ARMOR_BOOSTER			= "Boosters d'armure",	
+		ITEMTYPE_COSTUME				= "Costumes",		
+		ITEMTYPE_DISGUISE				= "Déguisement",		
+		ITEMTYPE_DRINK					= "Boissons",	
+		ITEMTYPE_FOOD					= "Nourritures",	
+		ITEMTYPE_AVA_REPAIR				= "Objets de réparation",		
+		ITEMTYPE_LOCKPICK				= "Crochets",		
+		ITEMTYPE_POTION					= "Potions",	
+		ITEMTYPE_POISON					= "Poisons",	
+		ITEMTYPE_RECIPE					= "Recettes",	
+		ITEMTYPE_SCROLL					= "Parchemins",	
+		ITEMTYPE_SIEGE					= "Armes de Sieges",	
+		ITEMTYPE_SOUL_GEM				= "Gemmes d'âmes",		
+		ITEMTYPE_TABARD					= "Tabards",	
+		ITEMTYPE_TROPHY					= "Trophés"	
 	}
 }
--- MulesManagement.lua
+++ MulesManagement.lua
@@ -12,27 +12,43 @@
 
  -- Global Vars
 MMVars = "MulesManagementVars"
-
+currentVersion = "2.0"
 
 othersElementsMM = {
-    ITEMTYPE_WEAPON,
-    ITEMTYPE_ARMOR
+    "ITEMTYPE_WEAPON",
+    "ITEMTYPE_WEAPON_BOOSTER",
+    "ITEMTYPE_ARMOR",
+    "ITEMTYPE_ARMOR_BOOSTER",
+    "ITEMTYPE_COSTUME",
+    "ITEMTYPE_DISGUISE",
+    "ITEMTYPE_DRINK",
+    "ITEMTYPE_FOOD",
+    "ITEMTYPE_AVA_REPAIR",
+    "ITEMTYPE_LOCKPICK",
+    "ITEMTYPE_POTION",
+    "ITEMTYPE_POISON",
+    "ITEMTYPE_RECIPE",
+    "ITEMTYPE_SCROLL",
+    "ITEMTYPE_SIEGE",
+    "ITEMTYPE_SOUL_GEM",
+    "ITEMTYPE_TABARD",
+    "ITEMTYPE_TROPHY"
 }
 craftingElementsMM = {
-    CRAFTING_TYPE_BLACKSMITHING,
-    CRAFTING_TYPE_CLOTHIER,
-    CRAFTING_TYPE_ENCHANTING,
-    CRAFTING_TYPE_ALCHEMY,
-    CRAFTING_TYPE_PROVISIONING,
-    CRAFTING_TYPE_WOODWORKING
+    "CRAFTING_TYPE_RAW",
+    "CRAFTING_TYPE_BLACKSMITHING",
+    "CRAFTING_TYPE_CLOTHIER",     
+    "CRAFTING_TYPE_ENCHANTING",   
+    "CRAFTING_TYPE_ALCHEMY",      
+    "CRAFTING_TYPE_PROVISIONING", 
+    "CRAFTING_TYPE_WOODWORKING",  
+    "ITEMTYPE_STYLE_MATERIAL",
+    "ITEMTYPE_WEAPON_TRAIT",
+    "ITEMTYPE_ARMOR_TRAIT"
 }
 characterType = {
-    MM_CHARACTER_TYPE_MAIN,
-    MM_CHARACTER_TYPE_MULE
-}
-itemsFilter = {
-    MM_ITEMS_FILTER_ALL,
-    MM_ITEMS_FILTER_CRAFT
+    "MM_CHARACTER_TYPE_MAIN",
+    "MM_CHARACTER_TYPE_MULE"
 }
 langages = {
     "English",
@@ -44,7 +60,7 @@ MulesManagement = {}
 MulesManagement.Saved = {}
 
 local function placeItemsMM(fromBag, fromSlot, toBag, toSlot, maxDestQuantity)
-    --d("place items")
+    --d("[" .. fromBag .. "," .. fromSlot .."] => [" .. toBag .. "," .. toSlot .."] (" .. maxDestQuantity .. ")")
     ClearCursor()
     if CallSecureProtected("PickupInventoryItem", fromBag, fromSlot, maxDestQuantity) then
         CallSecureProtected("PlaceInInventory", toBag, toSlot)
@@ -57,19 +73,61 @@ local function getTranslated(text)
 end
 
 local function isMoveable(craftingType, itemType)
+    craftingTypeName = ""
+    itemTypeName = ""
+    if CRAFTING_TYPE_TRANSLATION_MM[craftingType] then
+        craftingTypeName = CRAFTING_TYPE_TRANSLATION_MM[craftingType]
+    end    
+    if ITEMTYPE_TRANSLATION_MM[itemType] then
+        d(itemType)
+        itemTypeName = ITEMTYPE_TRANSLATION_MM[itemType]
+    end
+
     --If the All filter is selectec
-    if MulesManagement.Saved.itemsFilter == getTranslated(MM_ITEMS_FILTER_ALL) then
+    if MulesManagement.Saved.all then
+        d("all")
         return true
     end
-    --If the craft filter is selected
-    if (craftingType ~= CRAFTING_TYPE_INVALID and MulesManagement.Saved[craftingType]) then
+    --CRAFT and RAW Material Test
+    if (craftingType ~= CRAFTING_TYPE_INVALID and MulesManagement.Saved[craftingTypeName]) then 
+        --if it's a raw material
+        if not (itemType == ITEMTYPE_ALCHEMY_BASE or itemType == ITEMTYPE_BLACKSMITHING_RAW_MATERIAL or itemType == ITEMTYPE_CLOTHIER_RAW_MATERIAL or itemType == ITEMTYPE_WOODWORKING_RAW_MATERIAL) then
+            d("wanted but not raw")
+            return true
+        elseif MulesManagement.Saved["CRAFTING_TYPE_RAW"] then
+            d("wanted and raw")
+            return true
+        else
+            d("not wanted and raw")
+            return false
+        end
+    end
+    --OTHERS
+    if (itemType ~= ITEMTYPE_NONE and MulesManagement.Saved[itemTypeName]) then
+        d("itemtype : " ..itemTypeName)
+        if craftingTypeName ~= nil then
+            d("craft :" .. craftingTypeName)
+        end
         return true
     end
     return false
 end
 
+local function displayChat(itemName, quantity, moved)
+    startString,endString = string.find(itemName,"%^")
+    itemName = string.sub(itemName,0,startString-1)
+    if MulesManagement.Saved["spamChat"] then
+        if moved then
+            d(quantity .. " " .. itemName .. " " .. getTranslated("itemsMoved"))
+        else
+            d(quantity .. " " .. itemName .. " " .. getTranslated("itemsStacked"))
+        end
+    end
+end
+
 local function moveItemsMM(paramFromBag, paramDestinationBag)
     nbItemsMove = 0
+    nbItemsStack = 0
     --types of bags
     destinationBag = paramDestinationBag
     fromBag        = paramFromBag
@@ -86,12 +144,13 @@ local function moveItemsMM(paramFromBag, paramDestinationBag)
     for slotDest = 0, bagSlots-1 do
         itemStack, itemMaxStack = GetSlotStackSize(destinationBag, slotDest)
         itemName = GetItemName(destinationBag, slotDest)
+        idItem   = GetItemInstanceId(destinationBag, slotDest)
         --if the slot is not empty
-        if(itemStack > 0 and itemName ~= nil) and itemStack < itemMaxStack then
-            itemsTables[itemName]           = {}
-            itemsTables[itemName].stack     = itemStack
-            itemsTables[itemName].maxStack  = itemMaxStack
-            itemsTables[itemName].slot      = slotDest
+        if(itemStack > 0 and itemName ~= nil and idItem ~= nil) and itemStack < itemMaxStack then
+            itemsTables[idItem]           = {}
+            itemsTables[idItem].stack     = itemStack
+            itemsTables[idItem].maxStack  = itemMaxStack
+            itemsTables[idItem].slot      = slotDest
         else
             table.insert(slotAvalaibleDest,slotDest)
         end
@@ -103,35 +162,60 @@ local function moveItemsMM(paramFromBag, paramDestinationBag)
     for slotFrom = 0, bagSlots-1 do
         itemStack, itemMaxStack = GetSlotStackSize(fromBag, slotFrom)
         itemName = GetItemName(fromBag, slotFrom)
+        idItem   = GetItemInstanceId(fromBag, slotFrom)
 
         --if the slot is not empty
-        if(itemStack > 0 and itemName ~= nil) then
+        if(itemStack > 0 and itemName ~= nil and idItem ~= nil) then
             --Get the crafting type
             usedInCraftingType, itemType, extraInfo1, extraInfo2, extraInfo3 = GetItemCraftingInfo(fromBag, slotFrom)
+            itemType = GetItemType(fromBag, slotFrom)
             --Check if we work on that item or not
-            if isMoveable(usedInCraftingType, itemType) then
-                --if the item is already present on the dest
-                if(itemsTables[itemName]) then
-                    destItem = itemsTables[itemName]
+            if itemsTables[idItem] or isMoveable(usedInCraftingType, itemType) then
+                --if the item is already present on the dest and its not a full stack
+                if(itemsTables[idItem]) and itemStack ~= itemMaxStack then
+                    destItem = itemsTables[idItem]
                     maxDestQuantity = destItem.maxStack - destItem.stack
                     --If there is enough place
+                    -- STACK ONLY --
                     if maxDestQuantity - itemStack >= 0 then
                         placeItemsMM(fromBag, slotFrom, destinationBag, destItem.slot, itemStack)
+                        --If there is just enough place
+                        if itemStack == itemsTables[idItem].itemMaxStack then
+                            table.remove(slotAvalaibleDest,idItem)
+                        else
+                            itemsTables[idItem].stack = itemsTables[idItem].stack + itemStack
+                        end
+                        nbItemsStack = nbItemsStack + 1
+                        displayChat(itemName, itemStack, false)
+                    -- STACK & MOVE --
                     else
                         --place as mush as we can then fill another spot
                         placeItemsMM(fromBag, slotFrom, destinationBag, destItem.slot, maxDestQuantity)
-                        nbItemsMove = nbItemsMove + 1
+                        nbItemsStack = nbItemsStack + 1
+                        displayChat(itemName, maxDestQuantity, false)
                         slotTmp = table.remove(slotAvalaibleDest,1)
                         if DoesBagHaveSpaceFor(destinationBag, fromBag, slotFrom) and slotTmp ~= nil then
                             placeItemsMM(fromBag, slotFrom, destinationBag, slotTmp, itemStack-maxDestQuantity)
+                            itemsTables[idItem].stack    = itemStack-maxDestQuantity
+                            itemsTables[idItem].slotDest = slotTmp
+                            nbItemsMove = nbItemsMove + 1
+                            displayChat(itemName, itemStack-maxDestQuantity, true)
                         end
                     end
                 --No item found in the destination
+                -- MOVE --
                 else
                     slotTmp = table.remove(slotAvalaibleDest,1)
                     if DoesBagHaveSpaceFor(destinationBag, fromBag, slotFrom) and slotTmp ~= nil then
                         placeItemsMM(fromBag, slotFrom, destinationBag, slotTmp, itemStack)
+                        if itemStack ~= itemMaxStack then
+                            itemsTables[idItem]           = {}
+                            itemsTables[idItem].stack     = itemStack
+                            itemsTables[idItem].maxStack  = itemMaxStack
+                            itemsTables[idItem].slot      = slotTmp
+                        end
                         nbItemsMove = nbItemsMove + 1
+                        displayChat(itemName, itemStack, true)
                     end
                 end
             end
@@ -139,7 +223,10 @@ local function moveItemsMM(paramFromBag, paramDestinationBag)
         end
 
     end
+    d("----------------------")
     d(nbItemsMove .. " " .. getTranslated("itemsMoved"))
+    d(nbItemsStack .. " " .. getTranslated("itemsStacked"))
+    d("----------------------")
 end
 
 function bankOpeningMM(eventCode, addOnName, isManual)
@@ -149,9 +236,9 @@ function bankOpeningMM(eventCode, addOnName, isManual)
 
     ClearCursor()
     --redirect if main or mules
-    if MulesManagement.Saved["characterType"] == getTranslated(MM_CHARACTER_TYPE_MAIN) then
+    if MulesManagement.Saved["characterType"] == getTranslated("MM_CHARACTER_TYPE_MAIN") then
         moveItemsMM(BAG_BACKPACK, BAG_BANK)
-    elseif MulesManagement.Saved["characterType"] == getTranslated(MM_CHARACTER_TYPE_MULE) then
+    elseif MulesManagement.Saved["characterType"] == getTranslated("MM_CHARACTER_TYPE_MULE") then
         moveItemsMM(BAG_BANK, BAG_BACKPACK)
     end
 end
@@ -164,17 +251,9 @@ local function changeLangageMM(val)
             d("keyCharacterType " .. keyTrad)
             d("translate " .. getTranslated(keyTrad))
             MulesManagement.Saved["characterType"] = getTranslated(keyTrad)
-        elseif MulesManagement.Saved["itemsFilter"] == tradValue then
-            d("keyItemsFilter " .. keyTrad)
-            d("translate " .. getTranslated(keyTrad))
-            MulesManagement.Saved["itemsFilter"]   = getTranslated(keyTrad)
         end
     end
 
-    ReloadUI()
-end
-local function changeItemsFilterMM(val)
-    MulesManagement.Saved["itemsFilter"] = val
     ReloadUI()
 end
 local function changeCharacterTypeMM(val)
@@ -191,54 +270,63 @@ local function getCharacterTypeList()
     end
     return characterTypeList
 end
-local function getItemsFilterList()
-    itemsFilterList = {}
-    for k,v in ipairs(itemsFilter) do
-        table.insert(itemsFilterList, getTranslated(v))
-    end
-    return itemsFilterList
-end
 
 
 local function optionsMM()
     local textCheckBox = ""
     local LAM = LibStub("LibAddonMenu-1.0")
     local optionsPanelMM = LAM:CreateControlPanel("Mules Management", "Mules Management")
-
-    LAM:AddHeader(optionsPanelMM, "headerMM", getTranslated("title"))
+    LAM:AddHeader(optionsPanelMM, "versionMM", "|c3366FF" .. getTranslated("version").."|r:" .. currentVersion)
+    LAM:AddHeader(optionsPanelMM, "headerMM", "|c3366FF" .. getTranslated("title").."|r" )
 
     LAM:AddDropdown(optionsPanelMM, "langageMM", getTranslated("dropDownLangageText"), getTranslated("dropDownLangageTooltip"), langages,
             function() return MulesManagement.Saved["langage"] end,
             changeLangageMM,
             true , getTranslated("reloadWarning"))
 
+    LAM:AddCheckbox(optionsPanelMM, "spamChat", getTranslated("spamChatText"), getTranslated("spamChatTooltip"),
+                function() return MulesManagement.Saved["spamChat"] end,
+                function(val) MulesManagement.Saved["spamChat"] = val end)
+    
     LAM:AddDropdown(optionsPanelMM, "characterTypeMM", getTranslated("dropDownCharacterTypeText"), getTranslated("dropDownCharacterTypeTooltip"), getCharacterTypeList(),
             function() return MulesManagement.Saved["characterType"] end,
             changeCharacterTypeMM,
             true , getTranslated("reloadWarning"))
 
-    LAM:AddDropdown(optionsPanelMM, "itemsFilterMM", getTranslated("dropDownItemsFilterText"), getTranslated("dropDownItemsFilterTooltip"), getItemsFilterList(),
-            function() return MulesManagement.Saved["itemsFilter"] end,
-            changeItemsFilterMM,
-            true , getTranslated("reloadWarning"))
+    
+    LAM:AddCheckbox(optionsPanelMM, "allMM", getTranslated("AllText"), getTranslated("AllTooltip"),
+                function() return MulesManagement.Saved["all"] end,
+                function(val) MulesManagement.Saved["all"] = val end)
 
-    --if it's craft mode !
-    if MulesManagement.Saved["itemsFilter"] == getTranslated(MM_ITEMS_FILTER_CRAFT) then
-    	for key,craftKey in pairs(craftingElementsMM) do
-            craftName = getTranslated(craftKey)
-            if MulesManagement.Saved["characterType"] == getTranslated(MM_CHARACTER_TYPE_MAIN) then
-                textCheckBox = craftName .. " - " .. getTranslated("craftCheckBoxTooltipMain")
-            elseif MulesManagement.Saved["characterType"] == getTranslated(MM_CHARACTER_TYPE_MULE) then
-                textCheckBox = craftName .. " - " .. getTranslated("craftCheckBoxTooltipMule")
-            end
-            --The checkbox
-            LAM:AddCheckbox(optionsPanelMM, craftKey .. craftName, craftName, textCheckBox,
-                function() return MulesManagement.Saved[craftKey] end,
-                function(val) MulesManagement.Saved[craftKey] = val end)
+
+    --CRAFT MODE
+    LAM:AddHeader(optionsPanelMM, "craftHeaderMM",  "|c3366FF" .. getTranslated("craftHeader").."|r")
+	for key,craftKey in pairs(craftingElementsMM) do
+        craftName = getTranslated(craftKey) .. ""
+        if MulesManagement.Saved["characterType"] == getTranslated("MM_CHARACTER_TYPE_MAIN") then
+            textCheckBox = craftName .. " - " .. getTranslated("checkBoxTooltipMain")
+        elseif MulesManagement.Saved["characterType"] == getTranslated("MM_CHARACTER_TYPE_MULE") then
+            textCheckBox = craftName .. " - " .. getTranslated("checkBoxTooltipMule")
         end
-    --if it's others filters mode
-    elseif MulesManagement.Saved["itemsFilter"] == getTranslated(MM_ITEMS_FILTER_OTHERS) then
+        --The checkbox
+        LAM:AddCheckbox(optionsPanelMM, craftKey .. craftName, craftName, textCheckBox,
+            function() return MulesManagement.Saved[craftKey] end,
+            function(val) MulesManagement.Saved[craftKey] = val end)    
+    end
 
+    --OTHERS MODE
+    LAM:AddHeader(optionsPanelMM, "othersHeaderMM", "|c3366FF" .. getTranslated("othersHeader").."|r")
+        for key,othersKey in pairs(othersElementsMM) do
+        othersKey = getTranslated(othersKey) .. ""
+        if MulesManagement.Saved["characterType"] == getTranslated("MM_CHARACTER_TYPE_MAIN") then
+            textCheckBox = othersKey .. " - " .. getTranslated("checkBoxTooltipMain")
+        elseif MulesManagement.Saved["characterType"] == getTranslated("MM_CHARACTER_TYPE_MULE") then
+            textCheckBox = othersKey .. " - " .. getTranslated("checkBoxTooltipMule")
+        end
+        --The checkbox
+        LAM:AddCheckbox(optionsPanelMM, othersKey .. othersKey, othersKey, textCheckBox,
+            function() return MulesManagement.Saved[othersKey] end,
+            function(val) MulesManagement.Saved[othersKey] = val end)    
     end
 end
 
@@ -249,15 +337,19 @@ function initMM(eventCode, addOnName)
 
     local defaults = {
         ["langage"]                 = "English",
-        ["characterType"]           = langage["English"][MM_CHARACTER_TYPE_MAIN],
-        ["itemsFilter"]             = langage["English"][MM_ITEMS_FILTER_CRAFT]
+        ["spamChat"]                = false,
+        ["characterType"]           = langage["English"]["MM_CHARACTER_TYPE_MAIN"],
+        ["all"]                     = false
     }
     
     for k,v in ipairs(craftingElementsMM) do
         defaults[v] = false
     end
-    
-    MulesManagement.Saved = ZO_SavedVars:New(MMVars, 1, nil, defaults, nil)
+    for k,v in ipairs(othersElementsMM) do
+        defaults[v] = false
+    end
+
+    MulesManagement.Saved = ZO_SavedVars:New(MMVars, 2, nil, defaults, nil)
     optionsMM()
     
     EVENT_MANAGER:RegisterForEvent("MulesManagement", EVENT_OPEN_BANK, bankOpeningMM)
